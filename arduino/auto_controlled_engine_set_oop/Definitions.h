cd6af138ee412af9e368ec6e8ba3f57f2ce87704
Sergey Volodin <etoestja@yandex.ru>
2013-07-14 19:14:27 +0400

    Comments in Definitions.h

--- arduino/auto_controlled_engine_set_oop/Definitions.h
+++ arduino/auto_controlled_engine_set_oop/Definitions.h
@@ -5,15 +5,22 @@
 
 #define doubleEps 1E-2
 
+// Do not wait for motors
 //#define DEBUG_NO_MOTORS
-//#define DEBUG_NO_GYROSCOPE
-//#define DEBUG_NO_ACCELEROMETER
-//#define DEBUG_NO_SERIAL
+
+// Add DAC8512 support and use it as InfoLED
 //#define DEBUG_DAC
+
+// Turn off tx to PC (used both in Arduino and Qt apps)
 //#define DEBUG_NO_TX_ARDUINO
 
+// Scale PID values (used both in Arduino and Qt apps)
 #define SERIAL_PID_COEFF 100
+
+// Scale AV values (used both in Arduino and Qt apps)
 #define SERIAL_GYRO_COEFF 8
+
+// Cut AV that is lower than this value (used in PID)
 #define PID_AV_MIN 3
 
 #endif
