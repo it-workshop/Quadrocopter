dccea0965df6348d604fcb3849487a24c553e5db
Sergey Volodin <etoestja@yandex.ru>
2014-01-12 13:51:21 +0400

    Voltage coeff, Joystick coeff

--- arduino/quadrocopter_oop/PWMJoystick.h
+++ arduino/quadrocopter_oop/PWMJoystick.h
@@ -4,8 +4,8 @@
 class PWMJoystick
 {
     static const int AVIndex = 3, AngleXIndex = 0, AngleYIndex = 1, PowerIndex = 2;
-    static const double AVCoeff = 3.14, AngleCoeff = 0.17;
-    static const double AVCoeffMin = 0.42;
+    static const double AVCoeff = 1.5, AngleCoeff = 0.25;
+    static const double AVCoeffMin = 0.25;
 public:
     PWMJoystick();
     double getAV();
--- arduino/quadrocopter_oop/Quadrocopter.h
+++ arduino/quadrocopter_oop/Quadrocopter.h
@@ -48,7 +48,7 @@ private:
 
     RVector3D angleManualCorrection;
 
-    static const double DefaultVSensorMaxVoltage = 15.16; //maximal voltage (before voltage divider)
+    static const double DefaultVSensorMaxVoltage = 17.95; //maximal voltage (before voltage divider)
     //15.16? 11.95->2.6
 
     static const double g = 9.80665; // gravitational acceleration
