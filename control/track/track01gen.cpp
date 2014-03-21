9bf29999b5334d994ac6c7bc5c9601a672a43983
Sergey Volodin <etoestja@yandex.ru>
2014-03-21 17:35:29 +0400

    good track

--- control/track/track01gen.cpp
+++ control/track/track01gen.cpp
@@ -3,34 +3,39 @@
 using std::cout;
 using std::endl;
 
+#define POWER 55
+#define rampUp(x, y, step) {for(i = x; i <= y; i += step) {cout << POWER << " " << i << " 0 0" << endl;}}
+#define rampDown(x, y, step) {for(i = x; i >= y; i -= step) {cout << POWER << " " << i << " 0 0" << endl;}}
+#define repeat(x, times) {for(i = 0; i < times; i++) {cout << POWER << " " << x << " 0 0" << endl;}}
+
 int main()
 {
     double i;
-    for(i = 0; i <= 45; i += 0.8)
-    {
-        cout << "36 " << i << " " << 0 << " " << 0 << endl;
-    }
+    repeat(0, 100);
 
-    for(i = 0; i < 10; i++)
-    {
-        cout << "36 45 0 0" << endl;
-    }
+    rampUp(0, 45, 0.8);
+    repeat(45, 10);
 
-    
-    for(i = 45; i >= -45; i -= 6)
-    {
-        cout << "36 " << i << " " << 0 << " " << 0 << endl;
-    }
+    rampDown(45, -45, 5);
+    repeat(-45, 10);
 
-    for(i = 0; i < 5; i++)
-    {
-        cout << "36 -45 0 0" << endl;
-    }
+    rampUp(-45, 45, 5);
+    repeat(45, 10);
 
-    for(i = -45; i >= 45; i += 6)
-    {
-        cout << "36 " << i << " " << 0 << " " << 0 << endl;
-    }
+    rampDown(45, -45, 5);
+    repeat(-45, 10);
+
+    rampUp(-45, 45, 5);
+    repeat(45, 10);
+
+    rampDown(45, -45, 3);
+    repeat(-45, 10);
+
+    rampUp(-45, 45, 3);
+    repeat(-45, 10);
+
+    rampDown(45, 0, 7);
+    repeat(0, 50);
 
     cout << "0 0 0 0" << endl;
 }
